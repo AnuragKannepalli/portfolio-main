20d044a470f574951dfc2e535d16523113545166
AnuragKannepalli <kannepalli.anurag@gmail.com>
2024-12-16 12:24:38 -0600

    df

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
+  base: '/portfolio-main/', // Replace 'portfolio' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
